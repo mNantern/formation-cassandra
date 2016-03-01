f99cd46bce2e87c5b500c4ec743cf65cdcb4e79a
Matthieu Nantern <mnantern@xebia.fr>
2016-03-01 11:14:14 +0100

    Refactoring IT structure
    # Conflicts:
    #	cassandra-iot/pom.xml

--- cassandra-iot/src/test/java/fr/xebia/training/BaseTest.java
+++ cassandra-iot/src/test/java/fr/xebia/training/BaseTest.java
@@ -16,10 +16,10 @@ public class BaseTest {
   public static final String LOCALHOST = "127.0.0.1";
   public static final String KEYSPACE = "cassandra_iot_test";
   public static final int CASSANDRA_PORT = 9042;
-  public Session session =  buildCassandraSession();
+  public Session session = buildCassandraSession();
   protected CQLDataLoader dataLoader = new CQLDataLoader(session);
 
-  public Session buildCassandraSession(){
+  public Session buildCassandraSession() {
     Session session = Cluster.builder()
         .addContactPoint(LOCALHOST)
         .withPort(CASSANDRA_PORT)
@@ -44,7 +44,7 @@ public class BaseTest {
     session.execute("USE " + KEYSPACE);
   }
 
-  protected void loadCQL(String filepath){
+  protected void loadCQL(String filepath) {
     dataLoader.load(new ClassPathCQLDataSet(filepath, false, false, KEYSPACE));
   }
 
